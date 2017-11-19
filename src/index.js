aaa3d4afe8bf7f2145982b5e8adb39c4b620bae4
zkan <kan@prontomarketing.com>
2017-11-19 13:35:20 +0700

    Add example of flow usage

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// @flow
+function concat(a: string, b: string): string {
+  return a + b;
+}
+
+console.log(concat('A', 'B'))
+
+// @flow
+function square(n: number): number {
+  return n * n;
+}
+
+console.log(square(2))
